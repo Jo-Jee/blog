9b0007302045f072e88b32c049e1a38cff034ff1
cozyband <capjjo@gmail.com>
2022-08-26 17:24:59 +0900

    imp: github, mail 로고 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,15 @@ export default function Footer() {
   return (
     <div className="border-t border-slate-200 flex flex-col space-y-2 py-5 mt-aut mt-5">
       <div className="flex space-x-1 max-w-4xl m-auto">
-        <Link href="/">
-          <a>github</a>
+        <Link href="https://github.com/cozyband">
+          <a>
+            <img src="/github.png"></img>
+          </a>
         </Link>
-        <Link href="/">
-          <a>mail</a>
+        <Link href="mailto:capjjo@gmail.com">
+          <a>
+            <img src="/mail.png"></img>
+          </a>
         </Link>
       </div>
     </div>
