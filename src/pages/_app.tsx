3564570280c1f9c9fca20103c4b853e353445829
cozyband <capjjo@gmail.com>
2022-08-26 17:15:05 +0900

    imp: title 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Layout from 'src/components/Layout'
 import '../../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>Cozyband 개발블로그</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
