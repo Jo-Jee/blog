7bf17e0c90915b8bc529095418d16ae17a89db15
cozyband <capjjo@gmail.com>
2022-08-30 11:56:32 +0900

    chore: Github 주소 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="border-t border-slate-200 flex flex-col space-y-2 py-5 mt-aut mt-5">
       <div className="flex space-x-1 max-w-4xl m-auto">
-        <Link href="https://github.com/cozyband">
+        <Link href="https://github.com/Jo-Jee">
           <a>
             <img src="/github.png"></img>
           </a>
