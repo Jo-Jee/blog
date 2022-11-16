8df7052d6cc83f0b5b0460bf929f24837d877e7a
Jo-Jee <george.park@bunjang.co.kr>
2022-11-16 17:52:12 +0900

    chore: code highlighting

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      typography: {
+      typography: ({ theme }) => ({
         DEFAULT: {
           css: {
             'code::after': false,
-            'code::before': false
+            'code::before': false,
+            code: {
+              'font-weight': 'normal',
+              padding: 4,
+              background: theme('colors.slate.200'),
+              'border-radius': '0.25rem',
+              margin: 3
+            }
+            // '--tw-prose-pre-code': theme('colors.black[300]')
           }
         }
-      }
+      })
     },
   },
   plugins: [
