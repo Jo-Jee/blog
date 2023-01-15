6943d6a6fa438b4bfaad9f5ca140cad00cf177b5
Jo-Jee <george.park@bunjang.co.kr>
2023-01-15 09:13:21 +0900

    pagenation 안된 api 사용하도록 변경

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -8,14 +8,14 @@ export default async function handler(
 ) {
   res.setHeader('Content-Type', 'text/xml')
 
-  const posts = await api.get<PostListResponse>('/posts')
+  const posts = await api.get<number[]>('/posts/ids')
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
                 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                ${posts.data.content
-                  .map((post) => {
+                ${posts.data
+                  .map((id) => {
                     return `
                     <url>
-                        <loc>${`https://blog.jojee.co.kr/${post.id}`}</loc>
+                        <loc>${`https://blog.jojee.co.kr/${id}`}</loc>
                     </url>
                     `
                   })
