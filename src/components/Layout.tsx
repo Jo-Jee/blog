e27d69e295ce2d6547a5ed064ace8bd8a58c77bd
Jo-Jee <capjjo@gmail.com>
2023-01-28 10:18:16 +0900

    body에 bg class 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import Menu from './Menu'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen items-stretch dark:bg-stone-800">
+    <div className="flex flex-col min-h-screen items-stretch">
       <Menu />
       <div className="container flex flex-col mx-auto px-5 max-w-4xl grow">
         {children}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="dark:bg-stone-800">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
