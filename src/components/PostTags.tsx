89cb6de79c5026e63c05496fc9a65b21912371c0
Jo-Jee <capjjo@gmail.com>
2023-02-02 22:44:49 +0900

    공통 css 분리

--- src/components/PostTags.tsx
+++ src/components/PostTags.tsx
@@ -7,9 +7,7 @@ export default function PostTags({ tags }: { tags: string[] }) {
         return (
           <Link href={`/tags/${tag}`} key={tag}>
             <a className="mx-1 first:ml-0">
-              <span className="rounded p-1 text-xs bg-slate-200 dark:bg-stone-600 dark:text-white">
-                {tag}
-              </span>
+              <span className="tag">{tag}</span>
             </a>
           </Link>
         )
